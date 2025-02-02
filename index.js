15b3b82c0dceefff7a5dade43f2ecee141093c70
Andrija Juran <jurandrija645@gmail.com>
2025-02-02 13:11:04 +0100

    manifest and icon addition

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// chrome://extensions/
 let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -12,7 +13,6 @@ inputBtn.addEventListener("click", function() {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-     // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${myLeads[i]}'>
